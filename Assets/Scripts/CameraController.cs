4c0a4896a3721578f2715fa950913b71af6ac89f
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-03-14 17:59:50 -0400

    Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float offset = 5.0f;
+    public Transform player;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(player.position.x > this.transform.position.x + offset)
+        {
+            this.transform.position = new Vector3(player.position.x - offset, this.transform.position.y, this.transform.position.z);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(this.transform.position, new Vector2((this.transform.position.x + offset), this.transform.position.y));
+        Gizmos.DrawLine(this.transform.position, new Vector2((this.transform.position.x - offset), this.transform.position.y));
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float minX, maxX;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    public Transform minWalk, maxWalk;
+
+    private Animator animator;
+    private SpriteRenderer sRend;
+    private bool isGrounded = false;
+    private Rigidbody2D rBody;
+    private bool isRight = true;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        sRend = GetComponent<SpriteRenderer>();
+
+        rBody = GetComponent<Rigidbody2D>();
+        transform.position = new Vector3(minWalk.position.x, this.transform.position.y, this.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        animator.SetBool("Grounded", isGrounded);
+
+        if (isGrounded)
+        {
+            if (isRight)
+            {
+                sRend.flipX = false;
+                rBody.velocity = new Vector2(5, 0);
+
+                if (this.transform.position.x >= maxWalk.position.x)
+                {
+                    isRight = false;
+                }
+            }
+            if (!isRight)
+            {
+                sRend.flipX = true;
+                rBody.velocity = new Vector2(-5, 0);
+
+                if (this.transform.position.x <= minWalk.position.x)
+                    isRight = true;
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(new Vector3(minX, -3.0f, 0), new Vector3(maxX, -3.0f, 0));
+    }
+}
